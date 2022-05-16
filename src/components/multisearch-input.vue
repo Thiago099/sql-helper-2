8ff526ed2579cc0b6415baf8f06e28be3845657a
Thiago099 <thiago.kaique01@gmail.com>
2022-05-16 17:18:09 -0300

    [feature] component

--- src/components/multisearch-input.vue
+++ src/components/multisearch-input.vue
@@ -4,6 +4,7 @@ import { ref, toRefs, defineProps } from 'vue'
 
 const props = defineProps<{value:string}>()
 const { value } = toRefs(props)
+const selected = ref('database')
 
 </script>
 
@@ -11,9 +12,9 @@ const { value } = toRefs(props)
     <div class="search-group">
         <i class="fa fa-search"></i>
         <div class="input-group">
-            <button class="btn btn-secondary"><i class="fa fa-database"></i></button>
-            <button class="btn btn-secondary"><i class="fa fa-table"></i></button>
             <input type="text" class="form-control" @input="$emit('input',value)">
+            <button class="btn" :class="{'btn-primary':selected == 'database','btn-light':selected != 'database'}" @click="selected = 'database'"><i class="fa fa-database"></i></button>
+            <button class="btn" :class="{'btn-primary':selected == 'table','btn-light':selected != 'table'}" @click="selected = 'table'"><i class="fa fa-table"></i></button>
         </div>
     </div>
 </template>
@@ -23,13 +24,17 @@ const { value } = toRefs(props)
     margin-bottom:10px;
 }
 .form-control{
-    padding-left:40px;
+    padding-left:35px;
 }
 .fa-search{
     color: rgb(177, 176, 176);
     position: fixed;
     margin-top: 10px;
     margin-left: 15px;
+    z-index: 100;
+}
+.btn-light{
+    border: 1px solid #ced4da;
 }
 
 </style>
