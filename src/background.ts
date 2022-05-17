60e9120d2abd4d8e0f5a444e96b08dbba5c56b4b
Thiago099 <tecnologiasthiago@gmail.com>
2022-05-17 18:50:23 -0300

    [feature] maxmize

--- src/background.ts
+++ src/background.ts
@@ -26,6 +26,7 @@ async function createWindow() {
     }
   })
   win.setMenu(null)
+  win.maximize()
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
