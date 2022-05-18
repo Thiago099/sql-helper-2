b339ff0f5f2eddf0b303260502dd99da675c35f9
Thiago099 <thiago.kaique01@gmail.com>
2022-05-18 12:57:51 -0300

    [refactor] promise

---
+++ src/views/main/foreign-key-finder.ts
@@ -0,0 +1,26 @@
+import { connection } from '@/libraries/mysql'
+
+export async function find_foreign_keys(table:string, database:string)
+{
+    return await new Promise((resolve) => {connection.value.query(`SELECT 
+        KEY_COLUMN_USAGE.TABLE_NAME,
+        KEY_COLUMN_USAGE.COLUMN_NAME,
+        KEY_COLUMN_USAGE.REFERENCED_TABLE_NAME,
+        KEY_COLUMN_USAGE.REFERENCED_COLUMN_NAME
+        FROM information_schema.TABLE_CONSTRAINTS
+        INNER JOIN information_schema.KEY_COLUMN_USAGE ON TABLE_CONSTRAINTS.CONSTRAINT_NAME = KEY_COLUMN_USAGE.CONSTRAINT_NAME
+        WHERE TABLE_CONSTRAINTS.CONSTRAINT_TYPE = 'FOREIGN KEY'
+        AND TABLE_CONSTRAINTS.TABLE_SCHEMA = '${database}'
+        AND KEY_COLUMN_USAGE.TABLE_SCHEMA = '${database}'
+        AND 
+        (
+            KEY_COLUMN_USAGE.TABLE_NAME = '${table}'
+            OR KEY_COLUMN_USAGE.REFERENCED_TABLE_NAME = '${table}'
+        )`,(err:any, rows:any)=>{
+            resolve({
+                child: rows.filter((item:any)=>item.TABLE_NAME == table),
+                parent: rows.filter((item:any)=>item.REFERENCED_TABLE_NAME == table)
+            })
+        })
+    })
+}
--- src/views/main/tableSelector.vue
+++ src/views/main/tableSelector.vue
@@ -1,28 +1,13 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { selected_tables } from './selected-tables'
-import { connection } from '@/libraries/mysql'
+import { find_foreign_keys } from './foreign-key-finder'
 
 for(const table of selected_tables.value)
 {
-    connection.value.query(`SELECT 
-        KEY_COLUMN_USAGE.TABLE_NAME,
-        KEY_COLUMN_USAGE.COLUMN_NAME,
-        KEY_COLUMN_USAGE.REFERENCED_TABLE_NAME,
-        KEY_COLUMN_USAGE.REFERENCED_COLUMN_NAME
-        FROM information_schema.TABLE_CONSTRAINTS
-        INNER JOIN information_schema.KEY_COLUMN_USAGE ON TABLE_CONSTRAINTS.CONSTRAINT_NAME = KEY_COLUMN_USAGE.CONSTRAINT_NAME
-        WHERE TABLE_CONSTRAINTS.CONSTRAINT_TYPE = 'FOREIGN KEY'
-        AND TABLE_CONSTRAINTS.TABLE_SCHEMA = '${table.database}'
-        AND KEY_COLUMN_USAGE.TABLE_SCHEMA = '${table.database}'
-        AND 
-        (
-            KEY_COLUMN_USAGE.TABLE_NAME = '${table.table.name}'
-            OR KEY_COLUMN_USAGE.REFERENCED_TABLE_NAME = '${table.table.name}'
-        )`,(err:any, rows:any)=>{
-            table.child = rows.filter((item:any)=>item.TABLE_NAME == table.table.name)
-            table.parent = rows.filter((item:any)=>item.REFERENCED_TABLE_NAME == table.table.name)
-        })
+    find_foreign_keys(table.table.name, table.database).then((result:any) => {
+        table.foreign_keys = result
+    })
 }
 
 </script>
@@ -34,8 +19,7 @@ for(const table of selected_tables.value)
                 <div class="group" style="height:calc(100vh - 70px);">
                     <div class="item" v-for="selected of selected_tables" :key="selected" > 
                         <span class="item-database">{{ selected.database }}</span>.<span class="item-table">{{ selected.table.name }}</span>
-                        {{selected.child}}
-                        {{selected.parent}}
+                        {{ selected.foreign_keys }}
                     </div>
                 </div>
             </div>
