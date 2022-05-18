0dbf4d8b5b859607850a1812e74b5a5ddde108f6
Thiago099 <thiago.kaique01@gmail.com>
2022-05-18 11:23:19 -0300

    [feature] debug

--- src/views/main/tableSelector.vue
+++ src/views/main/tableSelector.vue
@@ -3,7 +3,6 @@ import { ref } from 'vue'
 import { selected_tables } from './selected-tables'
 import { connection } from '@/libraries/mysql'
 
-const foreign_keys = ref<any[]>([])
 for(const table of selected_tables.value)
 {
     connection.value.query(`SELECT 
@@ -21,7 +20,8 @@ for(const table of selected_tables.value)
             KEY_COLUMN_USAGE.TABLE_NAME = '${table.table.name}'
             OR KEY_COLUMN_USAGE.REFERENCED_TABLE_NAME = '${table.table.name}'
         )`,(err:any, rows:any)=>{
-            table.foreign_keys = rows
+            table.child = rows.filter((item:any)=>item.TABLE_NAME == table.table.name)
+            table.parent = rows.filter((item:any)=>item.REFERENCED_TABLE_NAME == table.table.name)
         })
 }
 
@@ -29,13 +29,13 @@ for(const table of selected_tables.value)
 
 <template>
     <div>
-        {{foreign_keys}}
         <div class="row">
             <div class="col-6">
                 <div class="group" style="height:calc(100vh - 70px);">
                     <div class="item" v-for="selected of selected_tables" :key="selected" > 
                         <span class="item-database">{{ selected.database }}</span>.<span class="item-table">{{ selected.table.name }}</span>
-                        <!-- {{selected.foreign_keys}} -->
+                        {{selected.child}}
+                        {{selected.parent}}
                     </div>
                 </div>
             </div>
