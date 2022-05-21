846a5b01b74dd92e55be240e28c40248b48d95c8
Thiago099 <tecnologiasthiago@gmail.com>
2022-05-21 13:34:28 -0300

    [feature] search on table selector

--- src/views/main/tableSelector.vue
+++ src/views/main/tableSelector.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import MultiSearchInput from '@/components/multisearch-input.vue'
 
 import { selected_tables } from './selected-tables'
 import { find_foreign_keys } from './foreign-key-finder'
@@ -76,6 +78,12 @@ function is_valid(object:any) :any
     // find if there is duplicated
     return !(list.filter((item:any,index:number)=> list.indexOf(item) != index).length > 0)
 }
+const formSearchParent = ref('')
+const formSearchChild = ref('')
+
+const toSearchParent = ref('')
+const toSearchChild = ref('')
+
 </script>
 
 <template>
@@ -83,28 +91,62 @@ function is_valid(object:any) :any
     <div>
         <div class="row">
             <div class="col-6">
-                <div class="group" style="height:calc(100vh - 70px);">
-                    <div class="item-group text-center" v-if="selected_tables.filter(item=>!item.used).length > 0">Selected</div>
-                    <div class="item" draggable="true" v-for="selected of selected_tables.filter(item=>!item.used)" :key="selected" @click="use(selected)" > 
-                        <span class="item-database">{{ selected.database }}</span>.<span class="item-table">{{ selected.table.name }}</span>
+                <multi-search-input 
+                    @table="formSearchParent = $event"
+                    @field="formSearchChild = $event"
+                    :fields="[
+                        
+                        {
+                            name:'table',
+                            icon:'fa-puzzle-piece'
+                        },
+                        {
+                            name:'field',
+                            icon:'fa-rocket'
+                        }
+                    ]"
+                />
+                <div class="group" style="height:calc(100vh - 130px);">
+                    <div v-show="'selected'.includes(formSearchParent)">
+                        <div class="item-group text-center" v-if="selected_tables.filter(item=>!item.used && item.table.name.includes(formSearchChild)).length > 0">selected</div>
+                        <div class="item" draggable="true" v-for="selected of selected_tables.filter(item=>!item.used && item.table.name.includes(formSearchChild))" :key="selected" @click="use(selected)" > 
+                            <span class="item-database">{{ selected.database }}</span>.<span class="item-table">{{ selected.table.name }}</span>
+                        </div>
                     </div>
-                    <div v-for="item in available_foreign_keys" :key="item" v-show="!item.children.every(child=>child.used == true)">
+                    <div v-for="item in available_foreign_keys" :key="item" v-show="!item.children.every(child=>child.used == true || !child.item.name.includes(formSearchChild))&&item.name.includes(formSearchParent)" >
                         <div class="item-group text-center">{{item.name}}</div>
-                        <div class="item" draggable="true" v-for="selected of item.children.filter(item=>!item.used)" :key="selected" @click="use_chained(selected)" > 
+                        <div class="item" draggable="true" v-for="selected of item.children.filter(item=>!item.used && item.item.name.includes(formSearchChild))" :key="selected" @click="use_chained(selected)" > 
                             <span class="item-database">{{ selected.database }}</span>.<span class="item-table">{{ selected.item.name }}</span> <span :class="{'item-parent':selected.item.child,'item-child':!selected.item.child}">{{ selected.item.COLUMN_NAME }}</span>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="col-6">
-                <div class="group" style="height:calc(100vh - 70px);">
-                <div class="item-group text-center" v-if="used_tables.length > 0">Selected</div>
-                    <div class="item" draggable="true" v-for="(selected, index) of used_tables" :key="selected" @click="unuse(selected,index)" > 
-                        <span class="item-database">{{ selected.database }}</span>.<span class="item-table">{{ selected.table.name }}</span>
+                <multi-search-input 
+                    @table="toSearchParent = $event"
+                    @field="toSearchChild = $event"
+                    :fields="[
+                        
+                        {
+                            name:'table',
+                            icon:'fa-puzzle-piece'
+                        },
+                        {
+                            name:'field',
+                            icon:'fa-rocket'
+                        }
+                    ]"
+                />
+                <div class="group" style="height:calc(100vh - 130px);">
+                    <div v-show="'selected'.includes(toSearchParent)">
+                        <div class="item-group text-center" v-if="used_tables.filter(item=>item.table.name.includes(toSearchChild)).length > 0">Selected</div>
+                        <div class="item" draggable="true" v-for="(selected, index) of used_tables.filter(item=>item.table.name.includes(toSearchChild))" :key="selected" @click="unuse(selected,index)" > 
+                            <span class="item-database">{{ selected.database }}</span>.<span class="item-table">{{ selected.table.name }}</span>
+                        </div>
                     </div>
-                    <div v-for="item in used_chained" :key="item" v-show="item.children.length > 0">
+                    <div v-for="item in used_chained.filter(item=>item.name.includes(toSearchParent))" :key="item" v-show="item.children.filter(item=>item.item.name.includes(toSearchChild)).length > 0">
                         <div class="item-group text-center">{{item.name}}</div>
-                        <div class="item" :class="{'error-item':!selected.parent.used}" draggable="true" v-for="(selected, index) of item.children" :key="selected" @click="unuse_chined(item.children,selected,index)" > 
+                        <div class="item" :class="{'error-item':!selected.parent.used}" draggable="true" v-for="(selected, index) of item.children.filter(item=>item.item.name.includes(toSearchChild))" :key="selected" @click="unuse_chined(item.children,selected,index)" > 
                             <span class="item-database">{{ selected.database }}</span>.<span class="item-table">{{ selected.item.name }}</span> <span :class="{'item-parent':selected.item.child,'item-child':!selected.item.child}">{{ selected.item.COLUMN_NAME }}</span>
                         </div>
                     </div>
