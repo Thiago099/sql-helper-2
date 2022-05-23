41c45e570792e8aa1e908420ce19f9321b2ababa
Thiago099 <thiago.kaique01@gmail.com>
2022-05-23 13:20:11 -0300

    [fix] alias

--- src/views/main/resultView.vue
+++ src/views/main/resultView.vue
@@ -70,7 +70,7 @@ if(root)
 {
     const alias = root?.alias
     const has_alias = root?.alias != root?.table
-    result = `<span class="sql-syntax">SELECT</span> ${select} <span class="sql-syntax">FROM</span> <span class="item-table">${root.table}</span>${has_alias?' <span class="sql-syntax">AS</span> <span class="item-table">'+alias+'</span> ':''}<br>${join}`
+    result = `<span class="sql-syntax">SELECT</span> ${select} <span class="sql-syntax">FROM</span> \`<span class="item-table">${root.table}</span>\`${has_alias?' <span class="sql-syntax">AS</span> \`<span class="item-table">'+alias+'</span>\` ':''}<br>${join}`
 }
 
 const element = ref()
